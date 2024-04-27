6329d89cc582a2d08f44d29cf141c934bcccc58b
Mlie <emipa606@users.noreply.github.com>
2024-04-27 22:44:01 +0200

    Fixed the patch for hospitality

--- Source/HospitalityPatch/HospitalityPatch.cs
+++ Source/HospitalityPatch/HospitalityPatch.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using HarmonyLib;
-using RimQuest;
-using RimWorld;
 using Verse;
 
 namespace HospitalityPatch;
@@ -14,21 +10,5 @@ internal static class HospitalityPatch
     static HospitalityPatch()
     {
         new Harmony("Mlie.RimQuest.HospitalityPatch").PatchAll(Assembly.GetExecutingAssembly());
-    }
-}
-
-[HarmonyPatch(typeof(IncidentWorker_VisitorGroup), "GiveItems")]
-public class Prefix_IncidentWorker_VisitorGroup
-{
-    [HarmonyPrefix]
-    public static void Prefix(ref IEnumerable<Pawn> visitors)
-    {
-        if (visitors == null || !visitors.Any())
-        {
-            return;
-        }
-
-        var value = true;
-        HarmonyPatches.AddQuestGiver(visitors.ToList(), null, null, ref value);
     }
 }
---
+++ Source/HospitalityPatch/Prefix_IncidentWorker_VisitorGroup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using Hospitality;
+using RimQuest;
+using Verse;
+
+namespace HospitalityPatch;
+
+[HarmonyPatch(typeof(IncidentWorker_VisitorGroup), "GiveItems")]
+public class Prefix_IncidentWorker_VisitorGroup
+{
+    [HarmonyPrefix]
+    public static void Prefix(ref IEnumerable<Pawn> visitors)
+    {
+        if (visitors == null || !visitors.Any())
+        {
+            return;
+        }
+
+        var value = true;
+        HarmonyPatches.AddQuestGiver(visitors.ToList(), null, null, ref value);
+    }
+}
